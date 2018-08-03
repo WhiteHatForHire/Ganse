6c2c285c258a7214beb26dc4473766379db287a6
Maxwell Stephens <stephensm2011@gmail.com>
2018-08-03 23:45:16 +0000

    setting up theme

--- themes/understrap-master/page-home.php
+++ themes/understrap-master/page-home.php
@@ -1,13 +1,6 @@
 <?php
 /**
- * The template for displaying all pages.
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
- *
- * @package understrap
+ * Template Name: Home Page
  */
 
 get_header();
@@ -17,7 +10,7 @@ $container   = get_theme_mod( 'understrap_container_type' );
 ?>
 
 <div class="wrapper" id="page-wrapper">
-
+<h1>HOME</h1>
 	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
 
 		<div class="row">
