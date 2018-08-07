ae5b4741b831db97c1b7acfa2ddaf1ab82fb1e23
Maxwell Stephens <stephensm2011@gmail.com>
2018-08-07 22:01:41 +0000

    added footer

--- themes/understrap-master/footer.php
+++ themes/understrap-master/footer.php
@@ -13,43 +13,49 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <?php get_template_part( 'sidebar-templates/sidebar', 'footerfull' ); ?>
 
-<div class="wrapper" id="wrapper-footer">
 
-	<div class="<?php echo esc_attr( $container ); ?>">
-
-		<div class="row">
-
-			<div class="col-md-12">
-
-				<footer class="site-footer" id="colophon">
-
-					<div class="site-info">
-
-							<a href="<?php  echo esc_url( __( 'http://wordpress.org/','understrap' ) ); ?>"><?php printf( 
-							/* translators:*/
-							esc_html__( 'Proudly powered by %s', 'understrap' ),'WordPress' ); ?></a>
-								<span class="sep"> | </span>
-					
-							<?php printf( // WPCS: XSS ok.
-							/* translators:*/
-								esc_html__( 'Theme: %1$s by %2$s.', 'understrap' ), $the_theme->get( 'Name' ),  '<a href="'.esc_url( __('http://understrap.com', 'understrap')).'">understrap.com</a>' ); ?> 
+	<footer id="colophon" class="site-footer theme-blue-background-solid" role="contentinfo">
+		<div class="row ">
+			<div class="col-md-3">
+				<?php the_custom_logo(); ?>
 				
-							(<?php printf( // WPCS: XSS ok.
-							/* translators:*/
-								esc_html__( 'Version: %1$s', 'understrap' ), $the_theme->get( 'Version' ) ); ?>)
-					</div><!-- .site-info -->
+			</div>
+			
+			<div id="footer-address" class="col-md-3 text-white text-center height-100 ">
+				<address>
+					930 Red Rose Ct Ste 209 <br>
+					Lancaster, PA 17601-1981 <br>
+					Direct: 877-271-5520
+				</address>
+			</div>
+			
+			<div class="col-md-3" id="footer-socials">
+				<a href="#"><i class="fa fa-facebook-official"></i></a>
+				<a href="#"><i class="fa fa-twitter"></i></a>
+				<a href="#"><i class="fa fa-youtube"></i></a>
+				<a href="#"><i class="fa fa-google-plus-official"></i></a>
+				<a href="#"><i class="fa fa-envelope"></i></a>
+			</div>
+			
+			<div class="col-md-3">
+				<img class="footer-img" src="/wp-content/uploads/2018/07/smarthub_snip.jpg"></img>
+			</div>
+			
+		</div>
+	</footer><!-- #colophon -->
+	
+</div><!-- #page -->
 
-				</footer><!-- #colophon -->
 
-			</div><!--col end -->
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3 ">Site hand-crafted by
+  <a class="wcd text-white" href="webcontentdevelopment.com" target="_blank"> Web Content Development</a>
+  	| © 2018 Copyright:
+    <a href="#" class="text-white"> Jeremy Ganse</a>
+  </div>
+  <!-- Copyright -->
 
-		</div><!-- row end -->
 
-	</div><!-- container end -->
-
-</div><!-- wrapper end -->
-
-</div><!-- #page we need this extra closing tag here -->
 
 <?php wp_footer(); ?>
 
